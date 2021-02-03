29a54b2e4264e392129d3ae65da7853661914a61
Aaron Soules <asoules@gmail.com>
2021-02-03 13:42:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@ code --install-extension rebornix.ruby
 code --install-extension kaiwood.endwise
 code --install-extension github.vscode-pull-request-github
 code --install-extension sysoev.vscode-open-in-github
-code --install-extension ~/dotfiles/vscode/sorbet.vsix
