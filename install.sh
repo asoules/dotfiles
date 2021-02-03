11107de0c43c5299e0d62eeaab56eec243a460ec
Aaron Soules <asoules@gmail.com>
2021-02-03 09:50:26 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+code --install-extension rebornix.ruby
+code --install-extension kaiwood.endwise
+code --install-extension github.vscode-pull-request-github
+code --install-extension sysoev.vscode-open-in-github
+code --install-extension ~/dotfiles/vscode-extensions/sorbet.vsix
